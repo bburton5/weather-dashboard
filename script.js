a93206acac971a9d2fe39afd9d910dcc1b9f8774
bburton5 <bburton5@uw.edu>
2022-08-15 00:00:37 -0700

    search bar now works

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 var apiKey = "196df1c55f3df94443139756f2fc08c6";
 
-document.getElementById("city").addEventListener("input", function () {
+document.getElementById("btnGetWeather").addEventListener("click", function () {
   var city = document.querySelector("input").value;
-  console.log(city);
-
-  document
-    .getElementById("btnGetWeather")
-    .addEventListener("click", getWeather());
-});
-
-function getWeather() {
-  city = document.querySelector("input").value;
   console.log(city);
   var apiURLCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
   fetch(apiURLCoordinates)
@@ -44,38 +35,37 @@ function getWeather() {
     .catch(function () {
       console.log(Error);
     });
-}
 
-function displayWeather(resp) {
-  console.log(resp);
-  console.log(city);
-  var todaysWeather = document.querySelector(".today");
-  var todaysDate = new Date(resp.current.dt * 1000);
-  todaysWeather.innerHTML = `<div class="current-city">
-    Current Weather in ${city}  <br />
+  function displayWeather(resp) {
+    console.log(resp);
+    console.log(city);
+    var todaysWeather = document.querySelector(".today");
+    var todaysDate = new Date(resp.current.dt * 1000);
+    todaysWeather.innerHTML = `<div class="current-city">
+    Current Weather in ${city.value}  <br />
     on ${todaysDate} <br>
     <img src="http://openweathermap.org/img/wn/10d@4x.png"> <br>
     Temp: ${resp.current.temp} ℉<br />
     Wind: ${resp.current.wind_speed} mph<br />
     Humidity: ${resp.current.humidity}% <br />
     <div class="uvindex">UV Index: ${resp.current.uvi}<br /></div>`;
-  var uviValue = resp.current.uvi;
-  console.log(uviValue);
-  var grabUvi = document.getElementsByClassName("uvindex");
-  if (uviValue <= 2) {
-    grabUvi[0].classList.add("favorable");
-  } else if (uviValue <= 5) {
-    grabUvi[0].classList.add("moderate");
-  } else if (uviValue > 5) {
-    grabUvi[0].classList.add("severe");
-  }
-  var futureWeather = document.querySelector(".future");
-  futureWeather.innerHTML = resp.daily
-    .map((day, idx) => {
-      if (idx <= 4) {
-        console.log(day);
-        var dt = new Date(day.dt * 1000);
-        return `<div class="card" style="width: 18rem">
+    var uviValue = resp.current.uvi;
+    console.log(uviValue);
+    var grabUvi = document.getElementsByClassName("uvindex");
+    if (uviValue <= 2) {
+      grabUvi[0].classList.add("favorable");
+    } else if (uviValue <= 5) {
+      grabUvi[0].classList.add("moderate");
+    } else if (uviValue > 5) {
+      grabUvi[0].classList.add("severe");
+    }
+    var futureWeather = document.querySelector(".future");
+    futureWeather.innerHTML = resp.daily
+      .map((day, idx) => {
+        if (idx <= 4) {
+          console.log(day);
+          var dt = new Date(day.dt * 1000);
+          return `<div class="card" style="width: 18rem">
       <div class="card-body">
         <h5 class="card-title">${dt.toDateString()}</h5>
         <img src="http://openweathermap.org/img/wn/${
@@ -88,7 +78,8 @@ function displayWeather(resp) {
         </p>
       </div>
     </div>`;
-      }
-    })
-    .join("");
-}
+        }
+      })
+      .join("");
+  }
+});
